a8b5aca4f6d2bae582b82116d7fbaf8705d60d09
Kirill Kotkas <grim4ik@users.noreply.github.com>
2016-11-24 15:18:33 +0200

    added

--- page/edit.php
+++ page/edit.php
@@ -14,7 +14,7 @@
 	//kas kasutaja uuendab andmeid
 	if(isset($_POST["update"])){
 		
-		$Event->updatePerson(cleanInput($_POST["id"]), $Helper->cleanInput($_POST["age"]), $Helper->cleanInput($_POST["color"]));
+		$Event->updatePerson(cleanInput($_POST["id"]), $Helper->cleanInput($_POST["ad_price"]), $Helper->cleanInput($_POST["ad_text"]), $Helper->cleanInput($_POST["ad_name"]));
 		
 		header("Location: edit.php?id=".$_POST["id"]."&success=true");
         exit();	
@@ -33,10 +33,12 @@
 <h2>Muuda kirjet</h2>
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
 	<input type="hidden" name="id" value="<?=$_GET["id"];?>" > 
-  	<label for="age" >vanus</label><br>
-	<input id="age" name="age" type="text" value="<?php echo $p->age;?>" ><br><br>
-  	<label for="color" >värv</label><br>
-	<input id="color" name="color" type="color" value="<?=$p->color;?>"><br><br>
+	<label for="ad_name" >Toote nimetus</label><br>
+	<input id="ad_name" name="name" type="text" value="<?php echo $p->ad_name;?>" ><br><br>
+  	<label for="ad_price" >Hind</label><br>
+	<input id="ad_price" name="price" type="text" value="<?php echo $p->ad_price;?>" ><br><br>
+  	<label for="ad_text" >Kirjeldus</label><br>
+	<input id="ad_text" name="text" type="text" value="<?=$p->ad_text;?>"><br><br>
   	
 	<input type="submit" name="update" value="Salvesta">
   </form>
