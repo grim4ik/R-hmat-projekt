372d2e6edb1ef5b95e9bc1f6048669fcb071f3e3
Kirill Kotkas <grim4ik@users.noreply.github.com>
2016-12-01 13:56:40 +0200

    added news in login

--- page/login.php
+++ page/login.php
@@ -53,10 +53,51 @@
 		</div>
 		
 		<div class="col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-3">
+		
+		
 
 	</body>
 </html>
 </div>
 </div>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+require_once '/home/kirikotk/config.php'; // подключаем скрипт
+ 
+$link = mysqli_connect($serverHost, $serverUsername, $serverPassword, $database) 
+    or die("Ошибка " . mysqli_error($link)); 
+     
+$query ="SELECT ad_name, ad_price, ad_text FROM rt_ad";
+ 
+$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+if($result)
+{
+    $rows = mysqli_num_rows($result); // количество полученных строк
+     
+    echo "<table><tr><th>Toote nimetus</th><th>Hind</th><th>Kirjeldus</th></tr>";
+    for ($i = 0 ; $i < $rows ; ++$i)
+    {
+        $row = mysqli_fetch_row($result);
+        echo "<tr>";
+            for ($j = 0 ; $j < 3 ; ++$j) echo "<td>$row[$j]</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+     
+    // очищаем результат
+    mysqli_free_result($result);
+}
+ 
+mysqli_close($link);
+?>
+</body>
+</html>
+
 
 <?php require("../footer.php"); ?>
